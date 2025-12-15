2d337211fd918012e7a203e0f475ed1f8b1609e6
mrcoklat <thecokl4t@gmail.com>
2025-12-15 11:33:05 +0800

    feat: add sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import { ModeToggle } from "@/components/theme-toggle";
 export default function Home() {
   return (
     <div className="bg-background text-foreground">
-      <Button>Click me</Button>
-      <div className="bg-primary text-primary-foreground">Hello</div>
-      <div className="bg-warning text-warning-foreground">Warning!</div>
       <ModeToggle />
       <div />
     </div>
