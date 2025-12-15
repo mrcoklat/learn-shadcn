9a15ca2e1bb31f486b9a64234dd586c7212ed6cb
mrcoklat <thecokl4t@gmail.com>
2025-12-15 09:42:49 +0800

    feat: use css variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-background text-foreground">
       <Button>Click me</Button>
+      <div className="bg-primary text-primary-foreground">Hello</div>
+      <div className="bg-warning text-warning-foreground">Warning!</div>
+      <div />
     </div>
   );
 }
