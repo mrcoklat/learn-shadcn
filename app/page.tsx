afe9ad6db8d8bb4af6345ad7c0f56830318d3c88
mrcoklat <thecokl4t@gmail.com>
2025-12-15 09:51:54 +0800

    feat: add dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
-
+import { ModeToggle } from "@/components/theme-toggle";
 export default function Home() {
   return (
     <div className="bg-background text-foreground">
       <Button>Click me</Button>
       <div className="bg-primary text-primary-foreground">Hello</div>
       <div className="bg-warning text-warning-foreground">Warning!</div>
+      <ModeToggle />
       <div />
     </div>
   );
