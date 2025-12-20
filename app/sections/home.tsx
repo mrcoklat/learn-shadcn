84d35ef0a154b344f17aace4f655a60274fa81d0
mrcoklat <thecokl4t@gmail.com>
2025-12-20 16:49:37 +0800

    feat: replave FloatingLines to LiquidEther & change GridDistortion bg

--- app/sections/home.tsx
+++ app/sections/home.tsx
@@ -9,44 +9,47 @@ export default function Home() {
         <HeroBackground />
       </div>
       <div className="absolute inset-0 z-10 flex items-center justify-center pointer-events-none">
-        <div>
-          <div className="text-4xl font-bold flex items-center justify-start gap-2 mb-10">
-            <span>I'm Ajimar, doing</span>
-            <RotatingText
-              texts={["Frontend", "Backend", "Full Stack", "Mobile"]}
-              mainClassName="w-50 px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-              staggerFrom={"last"}
-              initial={{ y: "100%" }}
-              animate={{ y: 0 }}
-              exit={{ y: "-120%" }}
-              staggerDuration={0.025}
-              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-              transition={{ type: "spring", damping: 30, stiffness: 400 }}
-              rotationInterval={2000}
+        <div className="flex gap-8 items-center justify-center w-full">
+          <div>
+            <div className="text-4xl font-bold flex items-center justify-start gap-2 mb-10">
+              <span>I'm Ajimar, doing</span>
+              <RotatingText
+                texts={["Frontend", "Backend", "Full Stack", "Mobile"]}
+                mainClassName="w-50 px-2 sm:px-2 md:px-3 bg-chart-4 text-primary overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                staggerFrom={"last"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.025}
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
+            </div>
+            <TrueFocus
+              sentence="From paperwork_to_one-click operations."
+              separator="_"
+              manualMode={false}
+              blurAmount={5}
+              borderColor="purple"
+              animationDuration={0.5}
+              pauseBetweenAnimations={0.5}
+              className="text-xl mb-30"
             />
+            <div className="flex gap-6 pointer-events-auto">
+              <a href="#contact">
+                <button className="bg-foreground text-background px-6 py-2 rounded-full font-bold hover:bg-primary hover:text-primary-foreground transition duration-200 w-36">
+                  Contact Me
+                </button>
+              </a>
+              <a href="#projects">
+                <button className="border border-foreground text-foreground px-6 py-2 rounded-full font-bold bg-transparent hover:bg-foreground hover:text-background transition duration-200 w-36">
+                  Projects
+                </button>
+              </a>
+            </div>
           </div>
-          <TrueFocus
-            sentence="From paperwork_to_one-click operations."
-            separator="_"
-            manualMode={false}
-            blurAmount={5}
-            borderColor="purple"
-            animationDuration={0.5}
-            pauseBetweenAnimations={0.5}
-            className="text-xl mb-30"
-          />
-          <div className="flex gap-6 pointer-events-auto">
-            <a href="#contact">
-              <button className="bg-foreground text-background px-6 py-2 rounded-full font-bold hover:bg-primary hover:text-primary-foreground transition duration-200 w-36">
-                Contact Me
-              </button>
-            </a>
-            <a href="#projects">
-              <button className="border border-foreground text-foreground px-6 py-2 rounded-full font-bold bg-transparent hover:bg-foreground hover:text-background transition duration-200 w-36">
-                Projects
-              </button>
-            </a>
-          </div>
+          <div>{/* Another component goes here */}</div>
         </div>
       </div>
     </div>
--- components/home/HeroBackground.tsx
+++ components/home/HeroBackground.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import FloatingLines from "@/components/home/FloatingLines";
+import LiquidEther from "@/components/home/LiquidEther";
 import GridDistortion from "@/components/home/GridDistortion";
 import { useTheme } from "@/hooks/use-theme";
 
@@ -8,14 +8,22 @@ export default function HeroBackground() {
   const theme = useTheme(); // "light" | "dark"
 
   return theme === "dark" ? (
-    <FloatingLines
-      enabledWaves={["top", "middle", "bottom"]}
-      lineCount={[10, 15, 20]}
-      lineDistance={[8, 6, 4]}
-      bendRadius={5}
-      bendStrength={-0.5}
-      interactive
-      parallax
+    <LiquidEther
+      colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+      mouseForce={20}
+      cursorSize={100}
+      isViscous={true}
+      viscous={30}
+      iterationsViscous={32}
+      iterationsPoisson={32}
+      resolution={0.5}
+      isBounce={false}
+      autoDemo={true}
+      autoSpeed={0.5}
+      autoIntensity={2.2}
+      takeoverDuration={0.25}
+      autoResumeDelay={3000}
+      autoRampDuration={0.6}
     />
   ) : (
     <GridDistortion
